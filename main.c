3846801cc2e6fc2a91acd4b89d49acc14761d613
HOME Enrique Gil Piqueras <proyectosegp@gmail.com>
2025-04-26 15:52:31 +0200

    Writing steps in main

--- main.c
+++ main.c
@@ -2,7 +2,18 @@ int	main(int argc, char **argv)
 {
 	if (argc == 5)
 	{
-
+		/* <file1 command1 | command2 >file2
+		 *
+		 * These are the expected arguments for pipex
+		 *
+		 *
+		 * */
+	/*	*	* PARSE	*	*	*/
+		//1) file1 exists? path to command1? 
+			//argv[2][0] is a "|"?	
+			//What about command2 and file2?
+		//2)
+	/*	*	Next step	*	*/
 		return (0);//correct execution, end
 	}
 	return (1);//wrong execution
