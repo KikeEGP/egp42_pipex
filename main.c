8fcab20689cef879eadcccb014a24d35afeeecdc
Enrique Gil Piqueras <enrgil-p@c1r1s3.42madrid.com>
2025-06-22 14:15:59 +0200

    First try with fork

--- main.c
+++ main.c
@@ -6,16 +6,23 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 13:39:23 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/06/22 13:41:45 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/06/22 14:06:15 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "pipex.h"
+
 int	main(int argc, char **argv)
 {
+	pid_t	fork_pid;
+
 	if (argc == 5)
 	{
-		pid_t	fork_pid;
-
+		fork_pid = fork();
+		if (fork_pid == -1)
+			return(1);
+		if (fork_pid == 0)
+			ft_printf("We are in child of %s\n", argv[0]);
 		/* <file1 command1 | command2 >file2
 		 *
 		 * These are the expected arguments for pipex
@@ -28,8 +35,8 @@ int	main(int argc, char **argv)
 			//What about command2 and file2?
 		//2)
 	/*	*	Next step	*	*/
-		return (0);//correct execution, end
+		if (fork_pid > 0)
+			return (0);//correct execution, end
 	}
 	return (1);//wrong execution
-
 }
--- pipex.h
+++ pipex.h
@@ -6,8 +6,15 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 13:40:19 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/06/22 13:41:37 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/06/22 14:03:41 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PIPEX_H
+# define PIPEX_H
 
+# include "libft/complete_libft.h"
+# include <unistd.h>
+# include <sys/wait.h>
+
+#endif
