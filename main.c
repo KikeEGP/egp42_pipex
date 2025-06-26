f8446d3fb9146157b7a7f6769a86c83330a935fc
Enrique Gil Piqueras <enrgil-p@c1r17s2.42madrid.com>
2025-06-26 21:52:26 +0200

    Implemented close_fds_and_set_error(), norminette says it is okay

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 13:39:23 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/06/25 18:03:32 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/06/26 21:51:30 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,28 +25,20 @@ static int	pipe_and_execute_cmds(t_pipex_data pipex_data)
 		error_happened(-1, "pipe");
 	pid_cmd_1 = fork();
 	if (pid_cmd_1 == -1)
-	{
-		close(pipe_fd[0]);
-		close(pipe_fd[1]);
-		error_happened(-1, "fork for cmd_1");
-	}
+		close_fds_and_set_error(pipe_fd[0], pipe_fd[1],
+			-1, "fork for cmd_1");
 	if (pid_cmd_1 == 0)
 		execute_cmd_1(pipex_data, pipe_fd);
 	pid_cmd_2 = fork();
 	if (pid_cmd_2 == -1)
-	{
-		close(pipe_fd[0]);
-		close(pipe_fd[1]);
-		error_happened(-1, "fork for cmd_2");
-	}
+		close_fds_and_set_error(pipe_fd[0], pipe_fd[1],
+			-1, "fork for cmd_2");
 	if (pid_cmd_2 == 0)
 		execute_cmd_2(pipex_data, pipe_fd);
 	close(pipe_fd[0]);
 	close(pipe_fd[1]);
 	waitpid(pid_cmd_1, &wstatus, 0);
-	waitpid(pid_cmd_2, &wstatus, 0);/*ALWAYS RETURN THE STATUS
-	OF SECOND WAITPID, EVEN IF WAITPID FAILS*//*Would work a if one of waitpid
-					== -1, error_happened()?*/
+	waitpid(pid_cmd_2, &wstatus, 0);
 	return (WEXITSTATUS(wstatus));
 }
 
