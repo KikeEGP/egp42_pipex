e80ea7a460996bf196bfe2c095468c030cbbaf19
Enrique Gil Piqueras <enrgil-p@c1r5s1.42madrid.com>
2025-06-27 20:39:10 +0200

    Change functions to cover all posible erros and obey norme

--- childs.c
+++ childs.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 19:07:36 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/06/27 19:51:40 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/06/27 20:36:35 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,29 +32,17 @@ void	execute_cmd_2(t_pipex_data pipex_data, int *pipe_fd)
 	int		fd_outfile;
 
 	if (close(pipe_fd[1]) == -1)
-	{
-		close(pipe_fd[0]);
-		error_happened(-1, "close pipe_fd[1] in cmd_1");
-	}
+		close_a_fd_and_error(pipe_fd[0], "close pipe_fd[0]" pipex_data);
 	fd_outfile = open(pipex_data.outfile, O_RDWR | O_CREAT | O_TRUNC, 0644);
-	if (fd_outfile == -1)//0644 why the zero?
-	{
-		close(pipe_fd[0]);
-		error_happened(-1, "No such file or directory");
-	}
+	if (fd_outfile == -1)
+		close_a_fd_and_error(pipe_fd[0],
+			"No such file or directory", pipex_data);
 	if (dup2(fd_outfile, 1) == -1 || dup2(pipe_fd[0], 0) == -1)
-	{
-		close(pipe_fd[0]);
-		close(fd_outfile);
-		error_happened(-1, "dup2() in cmd_1");
-	}
+		close_fds_and_error(fd_infile, pipe_fd[0], "dup2", pipex_data);
 	if (close(fd_outfile) == -1)
-	{
-		close(pipe_fd[0]);
-		error_happened(-1, "close fd_infile in cmd_1");
-	}
+		close_a_fd_and_error(pipe_fd[0], "close fd_outfile" pipex_data);
 	if (close(pipe_fd[0]) == -1)
-		error_happened(-1, "close pipe_fd[1] in cmd_1");
+		error_happened("close pipe_fd[1] in cmd_1", pipex_data);
 	path_to_execute = find_path(pipex_data.cmd_2[0], pipex_data.envp);
 	if (path_to_execute)
 	{
@@ -70,29 +58,17 @@ void	execute_cmd_1(t_pipex_data pipex_data, int *pipe_fd)
 	int		fd_infile;
 
 	if (close(pipe_fd[0]) == -1)
-	{//one fd and error
-		close(pipe_fd[1]);
-		error_happened(-1, "close pipe_fd[0] in cmd_1");
-	}
+		close_a_fd_and_error(pipe_fd[1], "close pipe_fd[1]" pipex_data);
 	fd_infile = open(pipex_data.infile, O_RDONLY);
 	if (fd_infile == -1)
-	{//one fd and error
-		close(pipe_fd[1]);
-		error_happened(-1, "No such file or directory");
-	}
+		close_a_fd_and_error(pipe_fd[1],
+			"No such file or directory", pipex_data);
 	if (dup2(fd_infile, 0) == -1 || dup2(pipe_fd[1], 1) == -1)
-	{//two fds and error
-		close(pipe_fd[1]);
-		close(fd_infile);
-		error_happened(-1, "dup2() in cmd_1");
-	}
+		close_fds_and_error(fd_infile, pipe_fd[1], "dup2", pipex_data);
 	if (close(fd_infile) == -1)
-	{//one fd and error
-		close(pipe_fd[1]);
-		error_happened(-1, "close fd_infile in cmd_1");
-	}
+		close_a_fd_and_error(pipe_fd[1], "close fd_infile" pipex_data);
 	if (close(pipe_fd[1]) == -1)
-		error_happened(-1, "close pipe_fd[1] in cmd_1");
+		error_happened("close pipe_fd[1] in cmd_1", pipex_data);
 	path_to_execute = find_path(pipex_data.cmd_1[0], pipex_data.envp);
 	if (path_to_execute)
 	{
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 13:39:23 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/06/27 19:29:09 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/06/27 20:30:29 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,30 @@
 
 /*Child will inherit *pipe_fd. In childs, close end which is not used.
  * But parent also will have to close both pipe_fdat the end*/
-static int	pipe_and_execute_cmds(t_pipex_data pipex_data)
+static int	pipe_and_execute_cmds(t_pipex_data pipex_data, int *pipe_fd)
 {
 	pid_t	pid_cmd_1;
 	pid_t	pid_cmd_2;
-	int		pipe_fd[2];
 	int		wstatus;
 
-	if (pipe(pipe_fd) == -1)
-		error_happened(-1, "pipe");//just message
 	pid_cmd_1 = fork();
 	if (pid_cmd_1 == -1)
-		close_fds_and_set_error(pipe_fd[0], pipe_fd[1],
-			-1, "fork for cmd_1");//two fds and message
+		close_fds_and_error(pipe_fd[0], pipe_fd[1], "fork", pipex_data);
 	if (pid_cmd_1 == 0)
 		execute_cmd_1(pipex_data, pipe_fd);
 	pid_cmd_2 = fork();
 	if (pid_cmd_2 == -1)
-		close_fds_and_set_error(pipe_fd[0], pipe_fd[1],
-			-1, "fork for cmd_2");//two fds and message
+		close_fds_and_error(pipe_fd[0], pipe_fd[1], "fork", pipex_data);
 	if (pid_cmd_2 == 0)
 		execute_cmd_2(pipex_data, pipe_fd);
-	close(pipe_fd[0]);//Protect this?
-	close(pipe_fd[1]);//Protect this?
-	waitpid(pid_cmd_1, &wstatus, 0);//Protect this?
-	waitpid(pid_cmd_2, &wstatus, 0);//Protect this?
+	if (close(pipe_fd[0]) == -1)
+		close_a_fd_and_error(pipe_fd[1], "close pipe_fd", pipex_data);
+	if (close(pipe_fd[1]) == -1)
+		error_happened("close pipe_fd", pipex_data);
+	if (waitpid(pid_cmd_1, &wstatus, 0) == -1)
+		error_happened("waitpid cmd_1", pipex_data);
+	if (waitpid(pid_cmd_2, &wstatus, 0) == -1)
+		error_happened("waitpid cmd_2", pipex_data);
 	return (WEXITSTATUS(wstatus));
 }
 
@@ -47,11 +46,20 @@ int	main(int argc, char **argv, char **enviroment_list)
 {
 	t_pipex_data	pipex_data;
 	int				status;
+	int				pipe_fd[2];
 
 	if (argc != 5)
-		error_happened(0, NULL);
+	{
+
+
+	}
+	if (pipe(pipe_fd) == -1)
+	{
+
+
+	}
 	storage_data(&pipex_data, argv, enviroment_list);
-	status = pipe_and_execute_cmds(pipex_data);
+	status = pipe_and_execute_cmds(pipex_data, pipe_fd);
 	free_cmd(pipex_data.cmd_1);
 	free_cmd(pipex_data.cmd_2);
 	return (status);
